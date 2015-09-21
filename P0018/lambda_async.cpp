6663a9fbe585993b54db5d0c2dc4ee0b39eba5c2
Carter Edwards <hcedwar@sandia.gov>
2015-09-21 10:34:31 -0600

    Refine the work-around for lamda capture of *this.

--- P0018/lambda_async.cpp
+++ P0018/lambda_async.cpp
@@ -8,6 +8,8 @@ private:
 
   int value ;
 
+  int get_value() const { return value ; }
+
 public:
 
   Work() : value(42) {}
@@ -17,6 +19,12 @@ public:
   {
     return std::async( [=]() -> int { return value ; } );
   }
+
+  std::future<int> spawn_2()
+  {
+    // Capture with initializer only for C++14
+    return std::async( [=,self=*this]() -> int { return self.get_value() ; } );
+  }
 };
 
 std::future<int> foo()
@@ -25,15 +33,30 @@ std::future<int> foo()
   return w.spawn();
 }
 
+std::future<int> foo_2()
+{
+  Work w ;
+  return w.spawn_2();
+}
+
 
 int main()
 {
-  std::future<int> f = foo();
-  std::cout << f.get() << std::endl ;
+  if ( 1 ) {
+    Work tmp ;
+    std::future<int> f = tmp.spawn();
+    std::cout << f.get() << std::endl ;
+  }
 
-  Work tmp ;
-  f = tmp.spawn();
-  std::cout << f.get() << std::endl ;
+  if ( 1 ) {
+    std::future<int> f = foo();
+    std::cout << f.get() << std::endl ;
+  }
+
+  if ( 1 ) {
+    std::future<int> f = foo_2();
+    std::cout << f.get() << std::endl ;
+  }
 
   return 0 ;
 }
